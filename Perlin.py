3cea9e316dc56f09a2ba59077e8a4849c22eab7f
Ali Madani <seyedali.madani@ucalgary.ca>
2023-02-28 14:39:03 -0700

    Porosity measurment added

--- Perlin.py
+++ Perlin.py
@@ -20,7 +20,7 @@ for y in range(height):
         perlin_noise[y][x] = snoise2(x/scale, y/scale, octaves=octaves, persistence=persistence, lacunarity=lacunarity)
 
 # Threshold the Perlin noise map to create a binary image
-threshold = 0.15
+threshold = 0
 binary_image = np.where(perlin_noise > threshold, 1, 0)
 binary_image = im.fromarray((binary_image * 255).astype(np.uint8))
 binary_image.save('2D.png')
@@ -31,9 +31,18 @@ image = cv2.imread('2D.png')
 # Apply the bilateral filter to smooth the edges
 averageBlur = cv2.blur(image, (5, 5))
 gaussian = cv2.GaussianBlur(image, (3, 3), 0)
+medianBlur = cv2.medianBlur(image, 9)
 os.remove('2D.png')
+#Porosity Calculation by Median blur filter image
+image_array = np.array(medianBlur)
+dimension=image_array.size
+blackpix=image_array.sum()
+Porosity=1-(blackpix/(255*dimension))
+print(Porosity)
+
 cv2.imshow('Raw image', image)
 cv2.imshow('Filtered image by Blur', averageBlur)
 cv2.imshow('Filtered image by Gaussian', gaussian)
+cv2.imshow('Filtered image by MedianBlur', medianBlur)
 cv2.waitKey()
 cv2.destroyAllWindows()
